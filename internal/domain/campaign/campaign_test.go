2bdd552ba0dd48e0bb8dca8502dfe3af910e73c1
DornelesArthur <arthhdorneles@gmail.com>
2025-04-08 21:34:42 -0300

    ✅ update new campaign test

--- internal/domain/campaign/campaign_test.go
+++ internal/domain/campaign/campaign_test.go
@@ -3,25 +3,20 @@ package campaign_test
 import (
 	"golang-email-sender/internal/domain/campaign"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
 func TestNewCampaign(t *testing.T) {
+	assert := assert.New(t)
 	name := "Campaing X"
 	content := "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis pharetra dolor non felis placerat luctus. Curabitur ac accumsan urna. Sed."
 	contacts := []string{"email1@gmail.com", "anotheremail3@yahoo.com", "thirdemail@hotmail.com"}
 
 	campaign := campaign.NewCampaign(name, content, contacts)
 
-	if campaign.ID != "1" {
-		t.Error("expected 1", campaign.ID)
-	}
-	if campaign.Name != name {
-		t.Error("expected the correct name")
-	}
-	if campaign.Content != content {
-		t.Error("expected the correct content")
-	}
-	if len(campaign.Contacts) != len(contacts) {
-		t.Error("expected the amount of contacts")
-	}
+	assert.Equal(campaign.ID, "1", "ID should 1")
+	assert.Equal(campaign.Name, name, "Name should be "+name)
+	assert.Equal(campaign.Content, content, "Content should be "+name)
+	assert.Equal(len(campaign.Contacts), len(contacts), "Number of Contacts dont match")
 }
