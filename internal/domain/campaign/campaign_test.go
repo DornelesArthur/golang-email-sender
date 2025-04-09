63a8d2760e6b4f5c4471d1d59d6e8e626990b943
DornelesArthur <arthhdorneles@gmail.com>
2025-04-08 21:59:52 -0300

    ✅ create new tests

--- internal/domain/campaign/campaign_test.go
+++ internal/domain/campaign/campaign_test.go
@@ -3,11 +3,12 @@ package campaign_test
 import (
 	"golang-email-sender/internal/domain/campaign"
 	"testing"
+	"time"
 
 	"github.com/stretchr/testify/assert"
 )
 
-func TestNewCampaign(t *testing.T) {
+func Test_NewCampaign_CreateNewCampaign(t *testing.T) {
 	assert := assert.New(t)
 	name := "Campaing X"
 	content := "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis pharetra dolor non felis placerat luctus. Curabitur ac accumsan urna. Sed."
@@ -15,8 +16,30 @@ func TestNewCampaign(t *testing.T) {
 
 	campaign := campaign.NewCampaign(name, content, contacts)
 
-	assert.Equal(campaign.ID, "1", "ID should 1")
 	assert.Equal(campaign.Name, name, "Name should be "+name)
 	assert.Equal(campaign.Content, content, "Content should be "+name)
 	assert.Equal(len(campaign.Contacts), len(contacts), "Number of Contacts dont match")
+}
+
+func Test_NewCampaign_IDIsNotNil(t *testing.T) {
+	assert := assert.New(t)
+	name := "Campaing X"
+	content := "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis pharetra dolor non felis placerat luctus. Curabitur ac accumsan urna. Sed."
+	contacts := []string{"email1@gmail.com", "anotheremail3@yahoo.com", "thirdemail@hotmail.com"}
+
+	campaign := campaign.NewCampaign(name, content, contacts)
+
+	assert.NotNil(campaign.ID, "ID shouldn'tbe nil")
+}
+
+func Test_NewCampaign_CreatedIsNotNil(t *testing.T) {
+	assert := assert.New(t)
+	name := "Campaing X"
+	content := "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis pharetra dolor non felis placerat luctus. Curabitur ac accumsan urna. Sed."
+	contacts := []string{"email1@gmail.com", "anotheremail3@yahoo.com", "thirdemail@hotmail.com"}
+	beforeCreated := time.Now()
+
+	campaign := campaign.NewCampaign(name, content, contacts)
+
+	assert.WithinRange(campaign.Created, beforeCreated, time.Now().Add(time.Minute))
 }
