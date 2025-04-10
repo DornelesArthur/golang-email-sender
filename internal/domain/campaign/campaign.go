1241d2dfb51e75a4700b706d7cf9f059abb7fa56
DornelesArthur <arthhdorneles@gmail.com>
2025-04-09 22:07:18 -0300

    ✅ add/update campaing tests

--- internal/domain/campaign/campaign.go
+++ internal/domain/campaign/campaign.go
@@ -1,6 +1,8 @@
 package campaign
 
 import (
+	"errors"
+	"net/mail"
 	"time"
 
 	"github.com/rs/xid"
@@ -18,10 +20,23 @@ type Campaign struct {
 	Contacts []Contact
 }
 
-func NewCampaign(name string, content string, emails []string) *Campaign {
+func NewCampaign(name string, content string, emails []string) (*Campaign, error) {
+	if name == "" {
+		return nil, errors.New("name is required")
+	}
+	if content == "" {
+		return nil, errors.New("content is required")
+	}
+	if len(emails) == 0 {
+		return nil, errors.New("email list is required")
+	}
 
 	contacts := make([]Contact, len(emails))
 	for index, email := range emails {
+		_, err := mail.ParseAddress(email)
+		if err != nil {
+			return nil, errors.New("email list contains invalid email")
+		}
 		contacts[index].Email = email
 	}
 
@@ -31,5 +46,5 @@ func NewCampaign(name string, content string, emails []string) *Campaign {
 		Content:  content,
 		Created:  time.Now(),
 		Contacts: contacts,
-	}
+	}, nil
 }
--- internal/domain/campaign/campaign_test.go
+++ internal/domain/campaign/campaign_test.go
@@ -9,15 +9,16 @@ import (
 )
 
 var (
-	name     = "Campaing X"
-	content  = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis pharetra dolor non felis placerat luctus. Curabitur ac accumsan urna. Sed."
-	contacts = []string{"email1@gmail.com", "anotheremail3@yahoo.com", "thirdemail@hotmail.com"}
+	name            = "Campaing X"
+	content         = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis pharetra dolor non felis placerat luctus. Curabitur ac accumsan urna. Sed."
+	contacts        = []string{"email1@gmail.com", "anotheremail3@yahoo.com", "thirdemail@hotmail.com"}
+	invalidContacts = []string{"email1@gmail.com", "anotheremail3@yahoo@.com", "thirdemailhotmail.com"}
 )
 
 func Test_NewCampaign_CreateNewCampaign(t *testing.T) {
 	assert := assert.New(t)
 
-	campaign := campaign.NewCampaign(name, content, contacts)
+	campaign, _ := campaign.NewCampaign(name, content, contacts)
 
 	assert.Equal(campaign.Name, name, "Name should be "+name)
 	assert.Equal(campaign.Content, content, "Content should be "+name)
@@ -27,7 +28,7 @@ func Test_NewCampaign_CreateNewCampaign(t *testing.T) {
 func Test_NewCampaign_IDIsNotNil(t *testing.T) {
 	assert := assert.New(t)
 
-	campaign := campaign.NewCampaign(name, content, contacts)
+	campaign, _ := campaign.NewCampaign(name, content, contacts)
 
 	assert.NotNil(campaign.ID, "ID shouldn'tbe nil")
 }
@@ -36,7 +37,39 @@ func Test_NewCampaign_CreatedMustBeNow(t *testing.T) {
 	assert := assert.New(t)
 	beforeCreated := time.Now()
 
-	campaign := campaign.NewCampaign(name, content, contacts)
+	campaign, _ := campaign.NewCampaign(name, content, contacts)
 
 	assert.WithinRange(campaign.Created, beforeCreated, time.Now().Add(time.Minute))
+}
+
+func Test_NewCampaign_ValidateNameIsEmpty(t *testing.T) {
+	assert := assert.New(t)
+
+	_, err := campaign.NewCampaign("", content, contacts)
+
+	assert.Equal("name is required", err.Error())
+}
+
+func Test_NewCampaign_ValidateContentIsEmpty(t *testing.T) {
+	assert := assert.New(t)
+
+	_, err := campaign.NewCampaign(name, "", contacts)
+
+	assert.Equal("content is required", err.Error())
+}
+
+func Test_NewCampaign_ValidateContactsIsEmpty(t *testing.T) {
+	assert := assert.New(t)
+
+	_, err := campaign.NewCampaign(name, content, []string{})
+
+	assert.Equal("email list is required", err.Error())
+}
+
+func Test_NewCampaign_ValidateContactsContainsInvalidEmail(t *testing.T) {
+	assert := assert.New(t)
+
+	_, err := campaign.NewCampaign(name, content, invalidContacts)
+
+	assert.Equal("email list contains invalid email", err.Error())
 }
