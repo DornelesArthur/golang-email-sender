6ddaa3ae6f4e589e7b66c33e6438362af9943264
DornelesArthur <arthhdorneles@gmail.com>
2025-08-14 00:28:42 -0300

    ✅ add new service tests

--- internal/domain/campaign/service_test.go
+++ internal/domain/campaign/service_test.go
@@ -1,6 +1,7 @@
 package campaign
 
 import (
+	"errors"
 	"golang-email-sender/internal/contract"
 	"testing"
 
@@ -17,38 +18,58 @@ func (r *repositoryMock) Save(campaign *Campaign) error {
 	return args.Error(0)
 }
 
-func Test_Create_Campaign(t *testing.T) {
-	assert := assert.New(t)
-
-	service := Service{}
-	newCampaign := contract.NewCampaign{
+var (
+	newCampaign = contract.NewCampaign{
 		Name:    "Test",
 		Content: "Body",
 		Emails:  []string{"test@gmail.com"},
 	}
+	repository = new(repositoryMock)
+	service    = Service{Repository: repository}
+)
+
+func Test_Create_Campaign(t *testing.T) {
+	assert := assert.New(t)
+
+	repository.On("Save", mock.Anything).Return(nil)
 
 	id, err := service.Create(newCampaign)
+
 	assert.NotNil(id)
 	assert.Nil(err)
 }
 
+func Test_Create_ValidateDomainError(t *testing.T) {
+	assert := assert.New(t)
+	newCampaign.Name = ""
+
+	_, err := service.Create(newCampaign)
+
+	assert.NotNil(err)
+	assert.Equal(err.Error(), "name is required")
+}
+
 func Test_CreateSave_Campaign(t *testing.T) {
-	newCampaign := contract.NewCampaign{
-		Name:    "Test",
-		Content: "Body",
-		Emails:  []string{"test@gmail.com"},
-	}
-	repositoryMock := new(repositoryMock)
-	repositoryMock.On("Save", mock.MatchedBy(func(campaign *Campaign) bool {
+
+	repository.On("Save", mock.MatchedBy(func(campaign *Campaign) bool {
 		if campaign.Name != newCampaign.Name || campaign.Content != newCampaign.Content || len(campaign.Contacts) != len(newCampaign.Emails) {
 			return false
 		}
 
 		return true
 	})).Return(nil)
-	service := Service{Repository: repositoryMock}
 
 	service.Create(newCampaign)
 
-	repositoryMock.AssertExpectations(t)
+	repository.AssertExpectations(t)
+}
+
+func Test_Create_ValidateRepositorySave(t *testing.T) {
+	assert := assert.New(t)
+
+	repository.On("Save", mock.Anything).Return(errors.New("error to save on database"))
+
+	_, err := service.Create(newCampaign)
+
+	assert.Equal("error to save on database", err.Error())
 }
