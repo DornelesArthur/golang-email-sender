4275a6cd6147ab0bf9294ccdb2880121675db2e6
DornelesArthur <arthhdorneles@gmail.com>
2025-09-06 21:14:58 -0300

    🚀 create api and first endpoints

---
+++ cmd/api/main.go
@@ -0,0 +1,16 @@
+package api
+
+import (
+	"net/http"
+
+	"github.com/go-chi/chi/v5"
+)
+
+func main() {
+	router := chi.NewRouter()
+
+	router.Get("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Hello World!"))
+	})
+	http.ListenAndServe(":3000", router)
+}
