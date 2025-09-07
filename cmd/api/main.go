db2b0ef67aed6509fb2d18fc56eba8861301875d
DornelesArthur <arthhdorneles@gmail.com>
2025-09-07 20:16:22 -0300

    feat: learn url and query params with chi

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,4 +1,4 @@
-package api
+package main
 
 import (
 	"net/http"
@@ -10,7 +10,17 @@ func main() {
 	router := chi.NewRouter()
 
 	router.Get("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("Hello World!"))
+		param := r.URL.Query().Get("name")
+		if param != "" {
+			w.Write([]byte(param))
+		} else {
+			w.Write([]byte("empty query data"))
+		}
+	})
+
+	router.Get("/{productName}", func(w http.ResponseWriter, r *http.Request) {
+		param := chi.URLParam(r, "productName")
+		w.Write([]byte(param))
 	})
 	http.ListenAndServe(":3000", router)
 }
