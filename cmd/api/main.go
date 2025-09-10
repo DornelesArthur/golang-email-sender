3f7cf3de3a50130a2d588e87c5eafbc85de64ad5
DornelesArthur <arthhdorneles@gmail.com>
2025-09-09 21:52:46 -0300

    feat: post method

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -8,6 +8,11 @@ import (
 	"github.com/go-chi/chi/v5"
 )
 
+type product struct {
+	ID   int
+	Name string
+}
+
 func main() {
 	router := chi.NewRouter()
 
@@ -28,6 +33,13 @@ func main() {
 	router.Get("/json", func(w http.ResponseWriter, r *http.Request) {
 		obj := map[string]string{"message": "success"}
 		render.JSON(w, r, obj)
+	})
+
+	router.Post("/product", func(w http.ResponseWriter, r *http.Request) {
+		var product product
+		render.DecodeJSON(r.Body, &product)
+		product.ID = 5
+		render.JSON(w, r, product)
 	})
 	http.ListenAndServe(":3000", router)
 }
