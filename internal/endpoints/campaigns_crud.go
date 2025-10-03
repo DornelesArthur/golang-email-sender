a15656c8b030562e684b8162c0a8411e6383655c
DornelesArthur <arthhdorneles@gmail.com>
2025-10-03 00:09:23 -0300

    ♻️ add global error handler

--- internal/endpoints/campaigns_crud.go
+++ internal/endpoints/campaigns_crud.go
@@ -1,34 +1,22 @@
 package endpoints
 
 import (
-	"errors"
 	"fmt"
 	"golang-email-sender/internal/contract"
-	internalerrors "golang-email-sender/internal/internalErrors"
 	"net/http"
 
 	"github.com/go-chi/render"
 )
 
-func (h *Handler) CampaignPost(w http.ResponseWriter, r *http.Request) {
+func (h *Handler) CampaignPost(w http.ResponseWriter, r *http.Request) (interface{}, int, error) {
 	fmt.Println(h)
 	var campaign contract.NewCampaign
 	render.DecodeJSON(r.Body, &campaign)
 	id, err := h.CampaignService.Create(campaign)
-	if err != nil {
-		if errors.Is(err, internalerrors.ErrInternal) {
-			render.Status(r, 500)
-		} else {
-			render.Status(r, 400)
-		}
-		render.JSON(w, r, map[string]string{"error": err.Error()})
-		return
-	}
-	render.Status(r, 201)
-	render.JSON(w, r, map[string]string{"id": id})
+	return map[string]string{"id": id}, 201, err
 }
 
-func (h *Handler) CampaignGet(w http.ResponseWriter, r *http.Request) {
-	render.Status(r, 200)
-	render.JSON(w, r, h.CampaignService.Repository.Get())
+func (h *Handler) CampaignGet(w http.ResponseWriter, r *http.Request) (interface{}, int, error) {
+	campaigns, err := h.CampaignService.Repository.Get()
+	return campaigns, 200, err
 }
